bcf0353b4e0c58c1d63abf1447d5ffdf89d4c988
Jose Inacio <38287810+InacioTi@users.noreply.github.com>
2024-09-15 12:04:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-file://%20malicious%20code%20to%20be%20executed%20on%20the%20serverconst%20fs%20=%20require('fs');fs.writefilesync('/tmp/exploited.txt',%20'CTF-BR%7Bexploit_success%7D');
+const fs = require('fs');
+fs.writeFileSync('/tmp/exploited.txt', 'CTF-BR{exploit_success}');
